75464932c735f834780252fd4a9142fceebc6005
Andrew Yakimtsev <126724863+andrew-yakimtsev@users.noreply.github.com>
2026-03-31 18:39:55 -0700

    Created a Weather API

---
+++ weather_api/main.py
@@ -0,0 +1,39 @@
+import requests, json, datetime
+from datetime import datetime
+
+today = datetime.now()
+formatted_date = today.strftime("%Y-%m-%d")
+
+def api_call():
+    url = "https://api.openweathermap.org/data/2.5/weather"
+
+    params = {
+        "lat": 38.5816,
+        "lon": -121.4944,
+        "appid": "",
+        "units": "imperial"
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    parsed_data = {
+        "date": datetime.now().strftime("%Y-%m-%d"),
+        "name": data["name"].title(),
+        "weather": data["weather"][0]["main"].title(),
+        "description": data["weather"][0]["description"].title(),
+        "temp": f"{data['main']['temp']}F"
+    }
+
+    print(f"Data successfully parsed!")
+    return parsed_data
+
+def print_json():
+    file_path = 'weather_api/weather_data.json'
+
+    with open(file_path, 'w') as json_file:
+        json.dump(api_call(), json_file, indent=4)
+
+print_json()
+
+#! TODO: Hide API Key, Don't Override Parse, Add "Run on startup (CRON)"
